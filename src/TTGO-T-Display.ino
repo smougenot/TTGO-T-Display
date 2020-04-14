dd2c85ef968724a230378dc7b774e3b23a184764
Antonio Vanegas <hpsaturn@gmail.com>
2020-04-14 14:24:48 +0200

    sync with master branch (deepsleep bug)

--- src/TTGO-T-Display.ino
+++ src/TTGO-T-Display.ino
@@ -6,7 +6,6 @@
 #include "esp_adc_cal.h"
 #include "bmp.h"
 
-
 #ifndef TFT_DISPOFF
 #define TFT_DISPOFF 0x28
 #endif
@@ -30,9 +29,9 @@ int btnCick = false;
 
 //! Long time delay, it is recommended to use shallow sleep, which can effectively reduce the current consumption
 void espDelay(int ms)
-{   
+{
     esp_sleep_enable_timer_wakeup(ms * 1000);
-    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH,ESP_PD_OPTION_ON);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
     esp_light_sleep_start();
 }
 
@@ -65,7 +64,11 @@ void button_init()
 
         tft.writecommand(TFT_DISPOFF);
         tft.writecommand(TFT_SLPIN);
-        esp_sleep_enable_ext1_wakeup(GPIO_SEL_35, ESP_EXT1_WAKEUP_ALL_LOW);
+        //After using light sleep, you need to disable timer wake, because here use external IO port to wake up
+        esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_TIMER);
+        // esp_sleep_enable_ext1_wakeup(GPIO_SEL_35, ESP_EXT1_WAKEUP_ALL_LOW);
+        esp_sleep_enable_ext0_wakeup(GPIO_NUM_35, 0);
+        delay(200);
         esp_deep_sleep_start();
     });
     btn1.setPressedHandler([](Button2 & b) {
@@ -123,44 +126,41 @@ void setup()
 {
     Serial.begin(115200);
     Serial.println("Start");
-    
-    Serial.println("tft init");
+
+    /*
+    ADC_EN is the ADC detection enable port
+    If the USB port is used for power supply, it is turned on by default.
+    If it is powered by battery, it needs to be set to high level
+    */
+    pinMode(ADC_EN, OUTPUT);
+    digitalWrite(ADC_EN, HIGH);
+
     tft.init();
     tft.setRotation(1);
-    Serial.println("tft to black");
     tft.fillScreen(TFT_BLACK);
-    Serial.println("tft text");
     tft.setTextSize(2);
-    tft.setTextColor(TFT_WHITE);
+    tft.setTextColor(TFT_GREEN);
     tft.setCursor(0, 0);
     tft.setTextDatum(MC_DATUM);
     tft.setTextSize(1);
 
-    Serial.println("tft backlight");
     if (TFT_BL > 0) { // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
-         pinMode(TFT_BL, OUTPUT); // Set backlight pin to output mode
-         digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+        pinMode(TFT_BL, OUTPUT); // Set backlight pin to output mode
+        digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
     }
 
-    Serial.println("tft swap bytes");
     tft.setSwapBytes(true);
-    Serial.println("tft push image");
     tft.pushImage(0, 0,  240, 135, ttgo);
     espDelay(5000);
 
-    Serial.println("tft colors");
     tft.setRotation(0);
-    int i = 5;
-    while (i--) {
-        tft.fillScreen(TFT_RED);
-        espDelay(1000);
-        tft.fillScreen(TFT_BLUE);
-        espDelay(1000);
-        tft.fillScreen(TFT_GREEN);
-        espDelay(1000);
-    }
+    tft.fillScreen(TFT_RED);
+    espDelay(1000);
+    tft.fillScreen(TFT_BLUE);
+    espDelay(1000);
+    tft.fillScreen(TFT_GREEN);
+    espDelay(1000);
 
-    Serial.println("button init");
     button_init();
 
     esp_adc_cal_characteristics_t adc_chars;
@@ -174,9 +174,17 @@ void setup()
     } else {
         Serial.println("Default Vref: 1100mV");
     }
+
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextDatum(MC_DATUM);
+    tft.drawString("LeftButton:", tft.width() / 2, tft.height() / 2 - 16);
+    tft.drawString("[WiFi Scan]", tft.width() / 2, tft.height() / 2 );
+    tft.drawString("RightButton:", tft.width() / 2, tft.height() / 2 + 16);
+    tft.drawString("[Voltage Monitor]", tft.width() / 2, tft.height() / 2 + 32 );
+    tft.drawString("RightButtonLongPress:", tft.width() / 2, tft.height() / 2 + 48);
+    tft.drawString("[Deep Sleep]", tft.width() / 2, tft.height() / 2 + 64 );
+    tft.setTextDatum(TL_DATUM);
 }
-
-
 
 void loop()
 {
